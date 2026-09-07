ecf810d2540f4adb46d95be09e265b674aefff5a
rajeshsaini-cup <rajeshsaini56908@gmail.com>
2026-09-07 16:03:15 +0530

    Navvar-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Home from './pages/Home';
 import SignUp from './SignUp';
 import SignIn from './Signin';
 import Profile from './Profile';
+import Header from './component/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+<Header/>
       <Routes>
+        
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/sign-up" element={<SignUp/>} />
